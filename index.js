984610caf35a88a2e465b123b628bd8e8f3900d7
CameronLumpkinATL <camlump@yahoo.com>
2020-07-14 13:40:06 -0400

    connected mongodb with mongoose

--- index.js
+++ index.js
@@ -2,9 +2,17 @@ const express = require('express');
 
 const app = express();
 
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb+srv://Cameron:Akimbo96@react-blog.7wsyc.mongodb.net/Blog?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+}).then(() =>console.log('DB connected')).catch(err => console.error(err));
+
 app.get('/', (req, res)=>{
     res.send('hello world')
 });
+
+
 
 app.listen(5000);
 
