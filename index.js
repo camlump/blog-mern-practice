f33fb9eb91edb465c02879fdea66b608320864d6
CameronLumpkinATL <camlump@yahoo.com>
2020-07-14 23:06:52 -0400

    setup ssh and started postman setup

--- index.js
+++ index.js
@@ -2,15 +2,26 @@ const express = require('express');
 
 const app = express();
 
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+
+const bodyParser = require('body-parser');
+
+const cookieParser = require('cookie-parser');
 
 mongoose.connect('mongodb+srv://Cameron:Akimbo96@react-blog.7wsyc.mongodb.net/Blog?retryWrites=true&w=majority', {
     useNewUrlParser: true
 }).then(() =>console.log('DB connected')).catch(err => console.error(err));
 
-app.get('/', (req, res)=>{
-    res.send('hello world')
-});
+// app.get('/', (req, res)=>{
+//     res.send('hello world')
+// });
+
+app.use(bodyParser.urlencoded({ extended : true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+
+
 
 
 
